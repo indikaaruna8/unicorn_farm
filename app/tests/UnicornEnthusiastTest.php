c3f253f649a330a0448a253e7e90ddfd72e8d537
Indika <hr.indikaaruna@gmail.com>
2023-03-21 16:34:11 +0530

    Create the test cases.

--- app/tests/UnicornEnthusiastTest.php
+++ app/tests/UnicornEnthusiastTest.php
@@ -1,15 +1,19 @@
 <?php
 
 namespace App\Tests;
+
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
 
 class UnicornEnthusiastTest extends ApiTestCase
 {
     public function testCreateUnicornEnthusiast(): void
-    {   
-        $email = sprintf("sepal%s@yahoo.com", rand(10000,10000333));
-        $response = static::createClient()->request('POST', '/api/unicorn_enthusiasts', [
-            "json" => [
+    {
+        $email = sprintf("sepal%s@yahoo.com", rand(10000, 10000333));
+        $response = static::createClient()->request(
+            'POST',
+            '/api/unicorn_enthusiasts',
+            [
+                "json" => [
                     "name" => "Sepal",
                     "email" =>  $email
                 ]
@@ -17,5 +21,62 @@ class UnicornEnthusiastTest extends ApiTestCase
         );
         $this->assertResponseStatusCodeSame(201);
         $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+    }
+
+    public function testPutUnicornEnthusiast(): void
+    {
+        $response = static::createClient()->request(
+            'PUT',
+            '/api/unicorn_enthusiasts/1',
+            [
+                "json" => [
+                    "name" => "Piter",
+                    "email" =>  'piter@yahoo.com'
+                ],
+
+            ]
+        );
+
+        $this->assertJsonContains([
+            "@type" => "UnicornEnthusiast",
+            "name" => "Piter",
+            "email" => "piter@yahoo.com"
+        ]);
+
+        echo $response->getContent();
+        $this->assertResponseStatusCodeSame(200);
+    }
+
+    public function testPatchUnicornEnthusiast(): void
+    {
+        $response = static::createClient()->request(
+            'PATCH',
+            '/api/unicorn_enthusiasts/1',
+            [
+                "json" => [
+                    "email" =>  'ronald@yahoo.com'
+                ],
+                'headers' => [
+                    'Accept' => 'application/ld+json',
+                    'Content-Type' => 'application/merge-patch+json'
+                ]
+            ]
+        );
+
+        $this->assertJsonContains([
+            "@type" => "UnicornEnthusiast",
+            "email" => "ronald@yahoo.com"
+        ]);
+
+        echo $response->getContent();
+        $this->assertResponseStatusCodeSame(200);
+    }
+    public function testDeleteUnicornEnthusiast(): void
+    {
+        $response = static::createClient()->request(
+            'DELETE',
+            '/api/unicorn_enthusiasts/1',
+        );
+        $this->assertResponseStatusCodeSame(204);
     }
 }
--- app/tests/UnicornTest.php
+++ app/tests/UnicornTest.php
@@ -31,24 +31,23 @@ class UnicornTest extends ApiTestCase
 
     public function testPagination(): void
     {
-         $response = static::createClient()->request('GET', '/api/unicorns?page=2');
+        $response = static::createClient()->request('GET', '/api/unicorns?page=2');
 
-         $this->assertResponseIsSuccessful();
-        // $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
-        // $this->assertJsonContains([
-        //     "@context" => "/api/contexts/Unicorn",
-        //     "@id" => "/api/unicorns",
-        //     "@type" => "hydra:Collection",
-        //     "hydra:totalItems" => 58,
-        //     "hydra:view" =>  [
-        //         "@id" => "/api/unicorns?page=2",
-        //         "@type" => "hydra:PartialCollectionView",
-        //         "hydra:first" => "/api/unicorns?page=1",
-        //         "hydra:last" => "/api/unicorns?page=2",
-        //         "hydra:previous" => "/api/unicorns?page=1"
-        //     ]
-        // ]);
-        // $this->assertCount(8, $response->toArray()['hydra:member']);
-        //$this->assertJsonContains(['@id' => '/']);
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertJsonContains([
+            "@context" => "/api/contexts/Unicorn",
+            "@id" => "/api/unicorns",
+            "@type" => "hydra:Collection",
+            "hydra:totalItems" => 58,
+            "hydra:view" =>  [
+                "@id" => "/api/unicorns?page=2",
+                "@type" => "hydra:PartialCollectionView",
+                "hydra:first" => "/api/unicorns?page=1",
+                "hydra:last" => "/api/unicorns?page=2",
+                "hydra:previous" => "/api/unicorns?page=1"
+            ]
+        ]);
+        $this->assertCount(8, $response->toArray()['hydra:member']);
     }
 }
